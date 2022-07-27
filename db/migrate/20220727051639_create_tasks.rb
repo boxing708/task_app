0c4749dc0ff5bf016b56185bc30c6f8e5a9d9de0
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-27 14:30:58 +0900

    generate task_model and rails db:migrate

---
+++ db/migrate/20220727051639_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.text :content, null: false
+      t.datetime :deadline, null: false
+      t.integer :status, null: false
+
+      t.timestamps
+    end
+  end
+end
