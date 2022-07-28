e64bfe69fb583e0c4450f8bfcc95364d6d3afa1e
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-28 15:03:51 +0900

    add icon strong_parameter to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
     devise_parameter_sanitizer.permit(:account_update, keys: [:name]) #  更新時(update時)にnameというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:account_update, keys: [:icon]) #  更新時(update時)にiconというキーのパラメーターを追加で許可する
   end
 end
