f91715027378fc92942b4abae5460b9831f95e27
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-28 18:33:45 +0900

    generate users_controller and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show ]
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def mypage
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+end
