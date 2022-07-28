9957a30be710f6a1592606a7a69376607f2a06ec
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-28 13:00:00 +0900

    add root to tasks#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root :to => "tasks#index"
   resources :tasks
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
