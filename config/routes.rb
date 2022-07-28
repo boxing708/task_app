d982c76172b53dd648c388eb43138369cf202e80
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-28 18:33:12 +0900

    add users/index show mypage routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root :to => "tasks#index"
+  devise_for :users
   resources :tasks
+  resources :users, :only => [:index, :show]
+  get "mypage" => "users#mypage"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
