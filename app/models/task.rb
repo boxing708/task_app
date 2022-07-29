fd4db5ea041565265e6239ebb59fcc3f4bd5c834
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-29 17:01:45 +0900

    add validates to task_model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :deadline, presence: true
+  validates :status, presence: true
   belongs_to :user
   paginates_per 10
   scope :index_all, -> {
