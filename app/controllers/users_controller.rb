1eda6180795d82ff747d0d1c9ef3564cced4cf7e
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-29 14:35:48 +0900

    resolve n + 1 problem in User.all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show ]
   def index
-    @users = User.all
+    @users = User.with_attached_icon
   end
 
   def show
