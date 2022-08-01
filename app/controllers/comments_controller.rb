6121b16b452b6d2f3c61ecc45f674c02c1b57b87
boxing708 <naoya.shishikura@super-studio.jp>
2022-08-01 15:30:22 +0900

    create create and destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,26 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @task = Task.find(params[:task_id])
+    @comment.task_id = @task.id
+
+    if @comment.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @task, status: :see_other
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :task_id)
   end
 end
