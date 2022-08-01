26a83a15165ac592c8ad7fb6707656d9a32d97f8
boxing708 <naoya.shishikura@super-studio.jp>
2022-08-01 11:05:36 +0900

    add before_action authenticate_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show ]
+  before_action :authenticate_user!
   def index
     @users = User.with_attached_icon
   end
