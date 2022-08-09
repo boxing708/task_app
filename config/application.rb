7dbbdbda8deae0cf408a2583758baaaca14c2ea3
boxing708 <naoya.shishikura@super-studio.jp>
2022-08-09 12:21:40 +0900

    エラー発生時のレイアウト崩れを修正。自動で読み込まれるfield_with_errorsタグを読み込まないように設定

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module TaskApp
     # config.time_zone = 'Tokyo'
     # config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     #
     # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
