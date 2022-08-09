e6b79e90b37787c5b91291194c7d920032e846b9
boxing708 <naoya.shishikura@super-studio.jp>
2022-08-09 16:08:02 +0900

    tasksテーブルにpriorityカラムをinteger型で作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_01_054958) do
+ActiveRecord::Schema.define(version: 2022_08_09_070311) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_08_01_054958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "priority", default: 0
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
