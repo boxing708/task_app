db6cbee346874d30f129e7f4aae1fffa3b1f084e
boxing708 <naoya.shishikura@super-studio.jp>
2022-08-09 17:26:42 +0900

    ストロングパラメーターでpriorityを許可

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -71,6 +71,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :content, :deadline, :status, :user_id)
+      params.require(:task).permit(:title, :content, :deadline, :status, :user_id, :priority)
     end
 end
