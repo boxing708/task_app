aae30eea0d8bfb57a0df3c01d883a38cbc88bbd3
boxing708 <naoya.shishikura@super-studio.jp>
2022-08-10 18:04:56 +0900

    アサイン時のフラッシュメッセージを修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   def assign_update
     if @task.update!(task_params)
       NoticeMailer.sendmail_task(@task).deliver
-      redirect_to task_url(@task), notice: t("flash.update", model: "タスク")
+      redirect_to task_url(@task), notice: "#{@task.user.name}さんにタスクがアサインされました。"
     end
 
   rescue ActiveRecord::RecordInvalid
