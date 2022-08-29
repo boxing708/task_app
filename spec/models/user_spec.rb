be5fb79b5c3ed98aa220dc1126ab9716deb685d5
boxing708 <naoya.shishikura@super-studio.jp>
2022-08-29 14:25:05 +0900

    [Add]Userモデルスペックを追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,40 @@ RSpec.describe User, type: :model do
     )
     expect(user).to be_valid
   end
+
+  it "メールがなければ無効な状態であること" do
+    user = User.new(
+      email: nil
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("が入力されていません。")
+  end
+
+  it "パスワードがなければ無効な状態であること" do
+    user = User.new(
+      password: nil
+    )
+    user.valid?
+    expect(user.errors[:password]).to include("が入力されていません。")
+  end
+
+  it "重複したメールアドレスなら無効な状態であること" do
+    user = User.create(
+      name: "Naoya",
+      email: "naoya@naoya.com",
+      password: "naoya0305",
+      password_confirmation: "naoya0305"
+    )
+    # user.confirm
+
+    user = User.new(
+      name: "Test",
+      email: "naoya@naoya.com",
+      password: "naoya0305",
+      password_confirmation: "naoya0305"
+    )
+
+    user.valid?
+    expect(user.errors[:email]).to include("は既に使用されています。")
+  end
 end
