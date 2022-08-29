2ffe8ef8f172e010d3318f072d4c41eaaa8976da
boxing708 <naoya.shishikura@super-studio.jp>
2022-08-29 18:48:42 +0900

    beforeで冗長なコードを修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,40 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it "タイトル、内容、期限、ステータスがあれば有効なこと" do
-    user = User.create(
+  before do
+    @user = User.create(
       name: "Naoya",
       email: "naoya@naoya.com",
       password: "naoya0305",
       password_confirmation: "naoya0305"
     )
 
-    task = user.tasks.create(
-      title: "タスク1",
-      content: "タスク1の内容",
-      deadline: "2022/09/08",
-      status: "未対応",
-    )
-
-    expect(task).to be_valid
-  end
-
-  it "優先度の翻訳ができていること" do
-    user = User.create(
-      name: "Naoya",
-      email: "naoya@naoya.com",
-      password: "naoya0305",
-      password_confirmation: "naoya0305"
-    )
-
-    task = user.tasks.create(
+    @task = @user.tasks.create(
       title: "タスク1",
       content: "タスク1の内容",
       deadline: "2022/09/08",
       status: "未対応",
       priority: 0,
     )
+  end
 
-    expect(task.translate_priority).to eq "低"
+  it "タイトル、内容、期限、ステータスがあれば有効なこと" do
+    expect(@task).to be_valid
+  end
+
+  it "優先度の翻訳ができていること" do
+    expect(@task.translate_priority).to eq "低"
   end
 end
