0364ff631f0a5b11183fcd246eb7af63be4b4c63
boxing708 <naoya.shishikura@super-studio.jp>
2022-08-31 16:04:05 +0900

    タスクモデルのバリデーションをテスト

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,22 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   before do
-    @user = FactoryBot.create(:user)
-
-    @task = @user.tasks.create(
-      title: "タスク1",
-      content: "タスク1の内容",
-      deadline: "2022/09/08",
-      status: "未対応",
-      priority: 0,
-    )
+    @task = FactoryBot.create(:task)
   end
 
-  it "タイトル、内容、期限、ステータスがあれば有効なこと" do
-    expect(@task).to be_valid
+  describe "バリデーションが機能していること" do
+    it "タイトル、内容、期限、ステータスがあれば有効なこと" do
+      expect(@task).to be_valid
+    end
+
+    it "タイトルがなければ無効な状態であること" do
+      @task.title = nil
+      @task.valid?
+      expect(@task.errors[:title]).to include("が空になっています。入力してください。")
+    end
+
+    it "内容がなければ無効な状態であること" do
+      @task.content = nil
+      @task.valid?
+      expect(@task.errors[:content]).to include("が空になっています。入力してください。")
+    end
+
+    it "期限がなければ無効な状態であること" do
+      @task.deadline = nil
+      @task.valid?
+      expect(@task.errors[:deadline]).to include("が空になっています。入力してください。")
+    end
+
+    it "ステータスがなければ無効な状態であること" do
+      @task.status = nil
+      @task.valid?
+      expect(@task.errors[:status]).to include("が空になっています。入力してください。")
+    end
   end
 
   it "優先度の翻訳ができていること" do
+    @task.priority = 0
     expect(@task.translate_priority).to eq "低"
   end
 end
