42f01a05ce3be39d1fd9051a9a6fdbda91439a4f
boxing708 <naoya.shishikura@super-studio.jp>
2022-09-07 11:25:01 +0900

    処理の順番修正 #13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show ]
   before_action :authenticate_user!
+  before_action :set_user, only: %i[ show ]
   before_action :set_q, only: %i[ mytask ]
 
   def index
